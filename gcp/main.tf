ebde421473ed0e73ae739e06a2a37d704c96eeae
Arrizki16 <kamildeka123@gmail.com>
2023-09-11 22:40:38 +0700

    terraform fmt

--- gcp/main.tf
+++ gcp/main.tf
@@ -8,47 +8,28 @@ resource "google_compute_instance_template" "instance-template" {
   name         = "rpl-instance-template"
   machine_type = "e2-medium"
   region       = "asia-southeast1"
-  
+
   network_interface {
     network = "default"
     access_config {
 
     }
   }
-  
+
   disk {
     source_image = "debian-cloud/debian-11"
   }
 }
 
-# resource "google_compute_instance" "rpl-instance" {
-#   name         = "rpl-instance"
-#   machine_type = "e2-medium"
-#   zone         = "asia-southeast1-a"
-  
-#   network_interface {
-#     network = "default"
-#     access_config {
-
-#     }
-#   }
-  
-#   boot_disk {
-#     initialize_params {
-#       image = "debian-cloud/debian-11"
-#     }
-#   }
-# }
-
-resource "google_compute_health_check" "autohealing" {
-  name                = "autohealing-health-check"
+resource "google_compute_health_check" "health-check" {
+  name                = "rpl-health-check"
   check_interval_sec  = 5
   timeout_sec         = 5
   healthy_threshold   = 2
   unhealthy_threshold = 10
 
   http_health_check {
-    request_path = "/healthz"
+    request_path = "/health-check"
     port         = "8080"
   }
 }
@@ -60,19 +41,9 @@ resource "google_compute_instance_group_manager" "instance-group-manager" {
   zone               = "asia-southeast1-a"
 
   version {
-    instance_template  = google_compute_instance_template.instance-template.self_link
+    instance_template = google_compute_instance_template.instance-template.self_link
   }
-
-  # all_instances_config {
-  #   metadata = {
-  #     metadata_key = "metadata_value"
-  #   }
-  #   labels = {
-  #     label_key = "label_value"
-  #   }
-  # }
-
-  target_size  = 2
+  target_size = 1
 
   named_port {
     name = "customhttp"
