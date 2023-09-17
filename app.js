5175183cddc3a0525a5fdd7a3163794e03ef0d5b
Arrizki16 <kamildeka123@gmail.com>
2023-09-17 22:41:45 +0700

    adding enpoint number 2 to login users

--- app.js
+++ app.js
@@ -1,14 +1,54 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = 1234;
 
 app.use(express.json());
 
-app.get('/api/version', (req, res) => {
-  const version = 'v16.14.2';
-  res.send(`Node version: ${version}`);
+const db = mysql.createConnection({
+  host: 'soon',
+  user: 'soon',
+  password: 'soon',
+  database: 'soon',
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to MySQL database');
+});
+
+app.post('/api/login', (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and password are required.' });
+  }
+
+  db.query(
+    'SELECT * FROM users WHERE email = ?',
+    [email],
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      if (results.length === 0) {
+        return res.status(401).json({ message: 'Incorrect email.' });
+      }
+
+      const user = results[0];
+
+      if (password === user.password) {
+        return res.status(200).json({ message: `Login successful. Welcome, ${user.username}!` });
+      } else {
+        return res.status(401).json({ message: 'Incorrect password.' });
+      }
+    }
+  );
 });
 
 app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
