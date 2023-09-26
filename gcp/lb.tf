bdf740f9185afad9c78c5baf2fa19945c1d0dd59
Arrizki16 <kamildeka123@gmail.com>
2023-09-26 23:08:37 +0700

    terraform plan no error for load balancer

--- gcp/lb.tf
+++ gcp/lb.tf
@@ -1,48 +1,62 @@
-resource "google_compute_forwarding_rule" "forwarding-rule" {
-  name                  = "forwarding-rule-test"
-  description           = "Forwarding rule for the Load Balancer"
-  target                = google_compute_target_http_proxy.default.id
-  port_range            = "80"
-  ip_protocol           = "TCP"
-  load_balancing_scheme = "EXTERNAL"
-  global                = true
-  ip_address            = google_compute_global_address.lb-ip.address
+resource "google_compute_global_address" "lb-ip" {
+  name        = "lb-ip"
+  description = "Global IP address for the Load Balancer"
+  ip_version  = "IPV4"
+  purpose     = "GLOBAL"
 }
 
-resource "google_compute_target_http_proxy" "default" {
-  name    = "http-proxy-test"
-  url_map = google_compute_url_map.default.id
-}
+resource "google_compute_health_check" "lb-health-check" {
+  name               = "lb-health-check-test"
+  check_interval_sec = 10
+  timeout_sec        = 5
 
-resource "google_compute_url_map" "default" {
-  name            = "url-map-test"
-  default_service = google_compute_backend_service.default.id
-
-  host_rule {
-    hosts        = ["mysite.com"]
-    path_matcher = "allpaths"
-  }
-
-  path_matcher {
-    name            = "allpaths"
-    default_service = google_compute_backend_service.default.id
-
-    path_rule {
-      paths   = ["/*"]
-      service = google_compute_backend_service.default.id
-    }
+  http_health_check {
+    request_path = "/"
+    port         = "80"
+    response     = "lb ok"
   }
 }
 
-resource "google_compute_backend_service" "default" {
+resource "google_compute_backend_service" "backend-service" {
   name        = "backend-service-test"
   port_name   = "http"
   protocol    = "HTTP"
   timeout_sec = 10
+  depends_on  = [google_compute_health_check.lb-health-check, google_compute_instance_group_manager.instance-group-manager]
 
   backend {
     group = google_compute_instance_group_manager.instance-group-manager.id
   }
 
-  health_checks = [google_compute_http_health_check.health-check.id]
+  health_checks = [google_compute_health_check.lb-health-check.id]
+}
+
+resource "google_compute_url_map" "url-map" {
+  name            = "url-map-test"
+  default_service = google_compute_backend_service.backend-service.id
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = google_compute_backend_service.backend-service.id
+
+    path_rule {
+      paths   = ["/*"]
+      service = google_compute_backend_service.backend-service.id
+    }
+  }
+}
+
+resource "google_compute_target_http_proxy" "http-proxy" {
+  name    = "http-proxy-test"
+  url_map = google_compute_url_map.url-map.id
+}
+
+resource "google_compute_forwarding_rule" "forwarding-rule" {
+  name                  = "forwarding-rule-test"
+  description           = "Forwarding rule for the Load Balancer"
+  target                = google_compute_target_http_proxy.http-proxy.id
+  port_range            = "80"
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  ip_address            = google_compute_global_address.lb-ip.address
 }
