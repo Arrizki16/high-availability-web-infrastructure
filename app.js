f1bcf7868ffae102ce8b434b6e9ff31467edc9c9
Stefanus Albert Kosim <albertkosim24@gmail.com>
2023-09-28 22:58:06 +0700

    add write to db endpoint

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config({path:'./.env'});
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
@@ -6,10 +7,11 @@ const port = 1234;
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: 'soon',
-  user: 'soon',
-  password: 'soon',
-  database: 'soon',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT)
 });
 
 db.connect((err) => {
@@ -53,6 +55,54 @@ app.post('/api/login', (req, res) => {
     }
   );
 });
+
+app.post('/api/register', (req, res) => {
+  const {username, email, password} = req.body;
+
+  if (!email || !password || !username) {
+    return res.status(400).json({ message: 'All fields must be filled.' });
+  }
+
+  db.query(
+    'SELECT * FROM users WHERE email = ? OR username = ?',
+    [email, username],
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      if (results.length === 0) {
+        db.query(
+          'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+          [username, email, password],
+          (err, results) => {
+            if (err) {
+              throw err;
+            }
+          }
+        );
+        return res.status(200).json({ message: `Account created.`});
+      }
+
+      if (results.length === 2) {
+        if (username === results[0].username || username === results[1].username) {
+          return res.status(401).json({ message: `Username ${username} already taken`});
+        }
+        if (email === results[0].email || email === results[1].email) {
+          return res.status(401).json({ message: `Email ${email} has been used`});
+        }
+      }
+
+      const user = results[0];
+
+      if (username === user.username) {
+        return res.status(401).json({ message: `Username ${username} already taken`});
+      } else {
+        return res.status(401).json({ message: `Email ${email} has been used`});
+      }
+    }
+  );
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
