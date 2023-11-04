b7f3fa8be32b7479c78c1ddb3995e283d2f8bc26
Stefanus Albert Kosim <albertkosim24@gmail.com>
2023-11-04 11:53:44 +0800

    update s3 iaac

--- aws/s3.tf
+++ aws/s3.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "rpl-s3-bucket" {
-  bucket = "rpl-s3-bucket"
+  bucket = "rpl-s3-bucket-for-final-project-research-rpl-2023"
+  force_destroy = true
   depends_on = [ aws_db_instance.db-rpl ]
 }
